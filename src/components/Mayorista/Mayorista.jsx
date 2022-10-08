ce5e11d532ffe389d92cd2cb4ebcd4070733523c
Mzrt1756 <98002264+Mzrt1756@users.noreply.github.com>
2022-10-08 14:45:37 -0300

    Página con Formulario de contacto

--- src/components/Mayorista/Mayorista.jsx
+++ src/components/Mayorista/Mayorista.jsx
@@ -1,8 +1,56 @@
+import { Container, Row, Col, Image } from "react-bootstrap"
+import { FaWhatsapp } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+import Logo from '../../assets/Coffee-shop-logo.png'
+import Img1 from '../../assets/cafe-latteart(1).jpeg'
+import Img2 from '../../assets/cafe-tostado(1).png'
+import "./Mayorista.css"
+
+
 export const Mayorista = () => {
     return(
         <div>
-            Mayorista:
-            <span>FORM</span>
+            <Container className="p-4 px-4 m-auto">
+                <Container className="py-3 px-5 border border-secondary border-opacity-50 d-flex justify-content-center">
+                        <Row>
+                            <div className="d-flex justify-content-center"><img className="imgLogoMay" src={Logo} alt="" /></div>
+                            <div className=" px-3"><h2>Soluciones para que puedas incorporar nuestro café de excelencia a tu negocio. </h2></div>
+                            <div className="d-flex justify-content-center fs-5 px-3"><p>Ya sea que tengas una cafetería, hotel, restaurante, oficina o plataforma de venta y distribución de café, en Ristretto Coffee House estamos para mejorar tu propuesta.
+        Canales exclusivos de venta mayorista:</p></div>
+                            <div className="fs-5 px-3"><p>Canales exclusivos de venta mayorista: </p></div>
+                            <Row>
+                                <Col lg className="m-2 text-center"><p className="fs-5"><FaWhatsapp/> + 54 9 221 555-5555</p></Col>
+                                <Col lg className="m-2 text-center"><p className="fs-5"><AiOutlineMail/> consultas@ristrettocoffee.com</p></Col>
+                            </Row>
+                        </Row>
+                </Container>
+                <Container>
+                    <Row className="pt-4">
+                        <Col lg className="d-flex justify-content-md-center">
+                            <Image fluid src={Img1} alt="tostadoraCafe" />
+                        </Col>
+                        <Col lg>
+                            <h4 className="tituloMayorista pb-2 pt-4">Selección</h4>
+                            <p className="textMayorista pb-2 ">Seleccionamos nuestros cafés de orígenes distintivos como Panamá, Etiopía, Colombia, Uganda y por supuesto Brasil, de donde viene nuestro café de la casa, que replicamos en varios locales. Nuestros proveedores son reconocidos cafeteros de la talla de Panama Varietals, Latorre & Dutch y OurCoffees Inc./Campos Altos.</p>
+                            <h4 className="tituloMayorista pb-2">Know-how</h4>
+                            <p className="textMayorista">Tostamos en nuestra máquina Probatone de 12kgs, de marca Probat, Alemana e importada de Austria. Nuestra Roaster Mercedes Salas vivió más de 13 años en el exterior y comenzó sus pasos tostando café en el famoso tostador austríaco 220Grad, donde se familiarizó con la técnica del tueste y el manejo de cafés de orígenes tan variados como los Panameños o Etíopes, pasando por los Brasileños.Desarrollamos curvas de tueste y perfiles de sabor únicos y pensados para los granos específicos que tostamos, teniendo en cuenta su terroir, cosecha, varietal, altitud y utilización proyectada. Utilizamos el software Cropster para poder ver cada curva de tueste y asegurar la consistencia necesaria para cada lote.</p>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                <Row className="pt-4">
+                        <Col lg className="colMayorista1">
+                            <h4 className="tituloMayorista pb-2 pt-4">Nuestros clientes</h4>
+                            <p className="textMayorista pb-2 ">Ya sea que tengas un restaurante, una cafetería, hotel, oficina o una plataforma de venta en la cual quieras sumar una experiencia verdaderamente distinta e incomparable, aquí estamos para ayudarte.</p>
+                            <h4 className="tituloMayorista pb-2">Servicio</h4>
+                            <p className="textMayorista pb-2">Te ofrecemos un servicio de post-venta ajustado a tus necesidades para que puedas mantener los estándares de calidad que nuestros cafés requieren y tus consumidores van a apreciar, en el día a día. Esto incluye capacitaciones a tu personal a través de nuestro departamento de calidad de café, nuestra roaster y nuestros baristas. Además, te daremos acceso a nuestras herramientas de trabajo y educación para que puedas consultarlas cuando sea necesario.</p>
+                        </Col>
+                        <Col lg className="colMayorista2 d-flex justify-content-md-center">
+                            <Image fluid src={Img2} alt="dibujoCafeconLeche" />
+                        </Col>
+                    </Row>
+                </Container>
+           </Container>
         </div>
     )
 }
--- src/components/Origenes/Origenes.jsx
+++ src/components/Origenes/Origenes.jsx
@@ -1,8 +1,57 @@
+import { Container, Row, Col, Image } from "react-bootstrap"
+import Img1 from '../../assets/origenes1.jpg'
+import Img2 from '../../assets/origenes2.jpg'
+import Img3 from '../../assets/origenes3.jpg'
+import Img4 from '../../assets/origenes4.jpeg'
+import "./Origenes.css"
+
 export const Origenes = () => {
     return(
         <div>
-            Origenes:
-            <span>FORM</span>
+            <Container className="p-4 pb-2 m-auto">
+                <Container><h2 className="text-center">Nuestros Orígenes</h2></Container>
+                <Container>
+                    <Row className="pt-4">
+                        <Col lg className="d-flex justify-content-md-center">
+                            <Image fluid className="imgOrigenes" src={Img1} alt="plantaCafe" />
+                        </Col>
+                        <Col lg className="d-flex">
+                            <p className="textOrigenes p-4 fs-4">“Trabajamos con cafés de especialidad, que no contienen defectos y presentan una trazabilidad al 100%, podemos definir estrategias para relucir el origen en tu taza.”</p>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                <Row >
+                        <Col lg className="colOrigenes1 d-flex">
+                            <p className="textOrigenes p-4">Comenzamos de la mano de Panamá Varietals, con las fincas Hartmann y Carmen Estate, unos productores muy reconocidos de cafés de especialidad panameños. Se seleccionaron e importaron decenas de bolsas caracterizadas por ser exóticas y exclusivas, difíciles de conseguir en un país pequeño que produce muchos de los cafés más finos y elegantes del mundo. 
+                            Panamá es el país que ha cambiado la mirada del mundo hacia el café, cuando la Finca o Hacienda La Esmeralda obtuvo precios récord en una subasta por el “Cup of excellence”, el mundo se dio cuenta de lo que ese país podía dar en términos de calidad.</p>
+                        </Col>
+                        <Col lg className="d-flex justify-content-md-center">
+                            <Image className="imgOrigenes" src={Img2} alt="cosechandoCafe" />
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                <Row>
+                        <Col lg className="d-flex justify-content-md-center">
+                            <Image className="imgOrigenes" fluid src={Img3} alt="granosTostadosCafe" />
+                        </Col>
+                        <Col lg className="d-flex">
+                            <p className="textOrigenes p-4">Lo que hace de Panamá un destino clave para la producción de cafés de extrema calidad, tiene que ver con sus microclimas y alturas, en conjunto con su recurso humano de alta calidad, que se vincula con la construcción del canal de Panamá y toda esa mano de obra calificada que luego continuó en distintas tareas, entre ellas, como caficultores.</p>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                <Row>
+                        <Col lg className="colOrigenes1 d-flex">
+                            <p className="textOrigenes p-4">También trabajamos con cafés de Brasil, de la región Cerrado de Minas Gerais. Una zona premiada por sus cafés y la primera que tuvo denominación de origen por las características únicas de su suelo. Los cafés brasileños se destacan por su extremo dulzor y por sus notas acarameladas, de chocolate y nueces. Son un excelente complemento de cafés donde la leche juega un papel sustancial, como el caso de un capuchino, latte o Flat White. Para llevar y como espresso especiales siempre contamos con algún café africano, Kenia o Etiopía, y también cafés colombianos. </p>
+                        </Col>
+                        <Col lg className="d-flex justify-content-md-center">
+                            <Image fluid className="imgOrigenes" src={Img4} alt="plantacionCafe" />
+                        </Col>
+                    </Row>
+                </Container>
+           </Container>
         </div>
     )
 }
