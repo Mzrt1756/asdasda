60047e4e125f8135e2e5a86d23f810affc69b6a5
Mzrt1756 <98002264+Mzrt1756@users.noreply.github.com>
2022-12-22 00:31:01 -0300

    Cambios en las descripciones y el texto

--- src/components/QuienesSomos/QuienesSomos.jsx
+++ src/components/QuienesSomos/QuienesSomos.jsx
@@ -8,12 +8,12 @@ export const QuienesSomos = () => {
                 <Row>
                     <Col><h2 className="textcenter pb-2">Nosotros</h2></Col>
                     <hr />
-                    <Col ><p className="fontNosotros fs-6 p-3">Ristretto Coffee House es un proyecto curado por más de 10 años y que nace del interés de Fidel y Ernesto en el café de especialidad en Australia, donde vivieron durante un año y descubrieron los famosos Flat Whites y brunches post-sesiones de surf en la mítica Bondi Beach de Sidney.</p>
-                    <p className="fontNosotros fs-6 px-3 pb-3">De ahí continuaron viajando por el mundo y estuvieron 10 años en Austria, donde Ernesto aprendió a tostar de la mano del reconocido tostador 220Grad, junto a Alois y Margret, sus mentores.</p></Col>
+                    <Col ><p className="fontNosotros fs-6 p-3">Ristretto Coffee House es un proyecto que comenzó hace más de 10 años y que nace del interés de Fidel y Ernesto en el café de especialidad durante su estadía en Nueva York, donde vivieron durante dos años antes de viajar a Centroamérica y recorrer cada pueblo cafetero por el que pasaron.</p>
+                    <p className="fontNosotros fs-6 px-3 pb-3">De ahí continuaron viajando por el mundo y estuvieron 5 años en Bélgica, donde Ernesto aprendió a tostar de la mano del reconocido tostador Jean Paul.</p></Col>
                     <hr />
-                    <Col><p className="fontNosotros fs-6 p-3">Ristretto Coffee House es un proyecto expansivo, con vocación de democratizar el café de excelencia. Al proyecto se le han sumado muchas personas que comparten el mismo horizonte: desde el apoyo inicial de Raúl, hasta el complemento de los socios Salvador, Nicolás y Darío, que hoy conforman un equipo que lleva adelante el día a día de nuestra misión.</p></Col>
+                    <Col><p className="fontNosotros fs-6 p-3">Ristretto Coffee House es un proyecto que busca masificar el consumo de café de especialidad, al hacerlo más accesible para todos. Al proyecto se le han sumado muchas personas que comparten la misma visión: desde el apoyo inicial de Raúl, hasta el complemento de los socios Salvador, Nicolás y Darío, que hoy conforman un equipo que lleva adelante el día a día de nuestra misión.</p></Col>
                     <hr />
-                    <Col><p className="fontNosotros fs-6 p-3">Contamos con nuestra Roastery, en Magdalena, donde funciona la casa madre de nuestro proyecto, tostándose café a diario y produciendose también panes de masa madre, croissants y otras cosas que hacen a nuestra propuesta como lo son los talleres y catas de café.</p></Col>
+                    <Col><p className="fontNosotros fs-6 p-3">Contamos con nuestra Roastery, en Magdalena, donde funciona la casa madre de nuestro proyecto, tostándose café a diario y produciéndose también panes de masa madre, croissants y otras cosas que hacen a nuestra propuesta como lo son los talleres y catas de café.</p></Col>
                 </Row>
            </Container>
         </div>
